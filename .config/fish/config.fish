d5dbfc4e7ada8542485bf6e631e45180b578728d
brhiro <49073956+brhiro@users.noreply.github.com>
2023-02-24 02:34:13 -0800

    feat(fish): change kitty background for neovim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,20 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Change kitty background (border) when opening and exiting nvim
+function nvim
+	kitty @ set-background-opacity 1.0
+	kitty @ set-colors background=#1e1e2e
+	/usr/bin/nvim $argv
+end
+
+function nvim_exit --on-event fish_postexec
+	set -gx neovim_running 0
+	and pgrep -x -P $fish_pid nvim >/dev/null ; set -gx neovim_running $status
+
+	if test $neovim_running -ne 0
+		kitty @ set-background-opacity 0.7
+		kitty @ set-colors background=#000000
+	end
+end
